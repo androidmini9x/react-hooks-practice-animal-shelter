dfd42c8792fa533045d49923012a02437be3dff6
androidmini9x <androidmini92@gmail.com>
2022-02-06 16:25:41 +0300

    Solved

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,30 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onFindPetsClick() {
+    const para = filters.type === "all" ? "pets" : "pets?type="+filters.type;
+
+    fetch("http://localhost:3001/"+para)
+    .then(resp => resp.json())
+    .then(data => {
+      setPets(data);
+    });
+  }
+
+  function onChangeType(e) {
+    setFilters({type: e.target.value});
+  }
+
+  function onAdoptPet(id) {
+    const newPets = pets.map(p => {
+      if(id === p.id)
+        return {...p, isAdopted: true}
+      else
+        return p;
+    });
+    setPets(newPets);
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +39,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
